04cb887b3d00aefa56ad44065b43a245c1f9b4ab
Amarjeet Singh <singhamarjeet0045@gmail.com>
2019-08-09 01:52:09 +0530

    after basic setup

---
+++ src/component/layout/Index.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+import Tracks from '../tracks/Tracks';
+class Index extends Component {
+    render() {
+        return (
+            <>
+            <Tracks />
+            </>
+        )
+    }
+}
+
+export default Index;
---
+++ src/component/tracks/Tracks.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+ class Tracks extends Component {
+    render() {
+        return (
+            <div>
+                <h2>The page is Tracks</h2>
+            </div>
+        )
+    }
+}
+
+export default Tracks;
